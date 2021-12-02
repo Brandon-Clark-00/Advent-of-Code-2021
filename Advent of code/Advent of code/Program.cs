a57cf83a91dfd861316bf520df7043b9571851e9
Brandon <byclark@dundee.ac.uk>
2021-12-02 16:07:27 +0000

    Completed day 2
    added input folder

--- Advent of code/Advent of code/Program.cs
+++ Advent of code/Advent of code/Program.cs
@@ -15,7 +15,7 @@ namespace Advent_of_code
         {
 
             Console.WriteLine("Init");
-            day1();
+            day2();
             Console.WriteLine("Complete");
 
         }
@@ -23,7 +23,7 @@ namespace Advent_of_code
         static void day1()
         {
             int counter = 0;
-            string[] input = File.ReadAllLines(@"C:\Users\Brandon\Documents\uni\Advent-of-Code-2021\Advent of code\Advent of code\Input.txt");
+            string[] input = File.ReadAllLines(@"../../../Inputs/1.txt");
             for (int i = 1; i < input.Length; i++)
             {
                 if (Int32.Parse(input[i]) > Int32.Parse(input[i - 1]))
@@ -47,6 +47,68 @@ namespace Advent_of_code
             }
 
             Console.WriteLine("Result for part 2:" + counter);
+        }
+        
+
+        static void day2()
+        {
+            string[] input = File.ReadAllLines(@"../../../Inputs/2.txt");
+            int horizontal = 0, depth = 0, aim = 0, total = 0;
+            foreach (var i in input)
+            {
+                string[] line = i.Split(' ');
+                switch (line[0])
+                {
+                    case "forward":
+                        horizontal += Int32.Parse(line[1]);
+                        break;
+                    case "up":
+                        depth -= Int32.Parse(line[1]);
+                        break;
+                    case "down":
+                        depth += Int32.Parse(line[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            total = horizontal * depth;
+
+            Console.WriteLine("Part 1:");
+            Console.WriteLine("Horizontal pos: " + horizontal);
+            Console.WriteLine("Depth pos: " + depth);
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("-------------------");
+
+            horizontal = 0;  depth = 0; aim = 0; total = 0;
+
+            foreach (var i in input)
+            {
+                string[] line = i.Split(' ');
+                switch (line[0])
+                {
+                    case "forward":
+                        horizontal += Int32.Parse(line[1]);
+                        depth += aim * Int32.Parse(line[1]);
+                        break;
+                    case "up":
+                        aim -= Int32.Parse(line[1]);
+                        break;
+                    case "down":
+                        aim += Int32.Parse(line[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            total = horizontal * depth;
+            Console.WriteLine("Part 2:");
+            Console.WriteLine("Horizontal pos: " + horizontal);
+            Console.WriteLine("Depth pos: " + depth);
+            Console.WriteLine("Aim: " + aim);
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("-------------------");
+
         }
     }
 }
